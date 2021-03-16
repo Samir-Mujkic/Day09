fcdb5031c9472b12c101edbf432765d38d5a4a5a
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-16 12:57:32 +0100

    zadatak 3

---
+++ ex03/src/index.js
@@ -0,0 +1,16 @@
+//Only change code below this line
+function myEquealFunction(num) {
+  if (num === 23) {
+    return "Equal";
+  }
+  return "Not equal";
+}
+console.log(myEquealFunction(5));
+console.log(myEquealFunction(23));
+console.log(myEquealFunction("23"));
+console.log(myEquealFunction("23"));
+console.log(myEquealFunction("text"));
+
+//Only change code above this line
+
+module.exports = myEquealFunction;
